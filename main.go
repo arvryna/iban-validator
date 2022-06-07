886027942e3d260194dd824f30283943df757e9f
arvryna <arv.pyrna@gmail.com>
2022-06-07 22:37:46 +0400

    Add an HTTP Post Endpoint for iban validation
    - [TDD] Just added endpoint to pass test

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/gin-gonic/gin"
 )
 
 func launchServer() {
+	server := gin.New()
+	server.Use(gin.Recovery()) // default Middlewhere that catches panic and returns 500 as default
+	server.POST("/validators/iban", func(ctx *gin.Context) {
+		ctx.JSON(400, gin.H{
+			"message": "Invalid Iban number",
+		})
+	})
 
+	server.Run(":9090")
 }
 
 func main() {
